9f1cc756917865046399aadd13db9e32f1438934
Priscila <priiica12@gmail.com>
2022-03-07 21:51:01 -0500

    Toggle menu added in mobile version

---
+++ Tasty/app.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const x = document.getElementById("topNavMobile");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
